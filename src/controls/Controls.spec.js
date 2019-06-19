d960fe02f1a5e1325ee27f4d737098008e84b2db
DwayneDevelops <DwayneDevelops@gmail.com>
2019-06-19 17:10:28 -0500

    MVP?

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,21 @@
-// Test away!
+import React from 'react';
+import Controls from './Controls.js';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each'
+
+describe('toggles', () => {
+    it('toggles closed gate', () => {
+        const { queryByText } = render(<Controls />);
+        const closeButton = queryByText(/close gate/i) 
+
+        expect(closeButton).toBeTruthy()
+    });
+
+    it('toggles open gate', () => {
+        const { queryByText } = render(<Controls closed= {true} />);
+        const openButton = queryByText(/open gate/i) 
+
+        expect(openButton).toBeTruthy()
+    });
+})
+
--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,35 @@
-// Test away
+import React from 'React';
+import Dashboard from './Dashboard.js';
+import { render } from '@testing-library/react';
+import '@testing-library/react/cleanup-after-each'
+// import renderer from 'react-test-renderer';
+
+describe('closed', () => {
+    it('renders unlocked', () => {
+        const { queryByText } = render(<Dashboard />);
+        const unlockedDisplay = queryByText(/unlocked/i);
+
+        expect(unlockedDisplay).toBeTruthy()
+    });
+
+    it('renders open', () => {
+        const { queryByText } = render(<Dashboard />);
+        const openDisplay = queryByText(/open/i);
+
+        expect(openDisplay).toBeTruthy()
+    });
+
+    it('renders lock gate', () => {
+        const { queryByText } = render(<Dashboard />);
+        const lockButton = queryByText(/lock gate/i);
+
+        expect(lockButton).toBeTruthy()
+    });
+
+    it('renders close gate', () => {
+        const { queryByText } = render(<Dashboard />);
+        const closeButton = queryByText(/close gate/i);
+
+        expect(closeButton).toBeTruthy()
+    });
+})
